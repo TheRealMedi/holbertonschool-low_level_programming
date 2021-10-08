51b7b9970c323ffced2c5e22f10699a5aa574de0
Andres Medina <andresmedinaph@gmail.com>
2021-10-08 18:57:58 -0500

    Adding 7th file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
- * 
- * 
+ * print_most_numbers - print numbers from 0 to 9.
+ * Return: void
  */
 void print_most_numbers(void)
 {
-    
+int a = 48;
+
+for (; a <= 57; a++)
+{
+if (a != 50 && a != 52)
+{
+_putchar(a);
+}
+}
+_putchar('\n');
 }
