3e3910f023bc5c9863878b728bc1f70416737c06
Andres Medina <andresmedinaph@gmail.com>
2021-10-07 21:04:25 -0500

    Adding 10th file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * Return: result
+ * @a: first integer to add
+ * @b: seconde integer
+ */
+int add(int a, int b)
+{
+int c;
+c = (a + b);
+return (c);
+}
