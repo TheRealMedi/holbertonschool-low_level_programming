cd28fda6155024cf6367d0d8c781ee9b03ddef96
Andres Medina <andresmedinaph@gmail.com>
2021-10-08 15:20:20 -0500

    Betty style

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * 
- * 
+ * _isupper - checks for uppercase character
+ * Return: returns 1 or 0
+ * @c: given value
  */
 int _isupper(int c)
 {
-    if ((c >= 65)&&(c <= 90))
-    {
-        return (1);
-    }
-    else
-    return (0);
+if ((c >= 65) && (c <= 90))
+{
+return (1);
+}
+else
+return (0);
 }
