550fea310ccbd26b87801d857c8cb66e2650084a
Andres Medina <andresmedinaph@gmail.com>
2021-10-11 22:19:31 -0500

    INIT Files

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main.h by Andrés Medina
+ */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+
+#endif
