2281373c5d56d7c511fadcdd82479c4553e0a66e
Andres Medina <andresmedinaph@gmail.com>
2021-10-12 20:54:18 -0500

    Solving struggles with pointer

---
+++ 0x05-pointers_arrays_strings/2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - Write a function that returns the length of a string.
+ * Return: Legth from the string
+ * @s: String wich his length be computed
+ */
+int _strlen(char *s)
+{
+int a = 0;
+
+for (; s[a] != '\0'; a++)
+{
+}
+return(a);
+}
