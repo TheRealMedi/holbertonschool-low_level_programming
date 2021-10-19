b1a0c73bdee0053c550d2f689b6d54032d76d7f6
Andres Medina <andresmedinaph@gmail.com>
2021-10-19 17:23:39 -0500

    Betty Styling

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * 
- * 
- * 
+ * print_chessboard - Prints the chessboard
+ * @a: pointer Array
+ * Return: void
  */
 void print_chessboard(char (*a)[8])
 {
 int i, j;
-for( i = 0; i < 8; i++) 
+for (i = 0; i < 8; i++)
 {
-for(j = 0; j < 8; j++)
+for (j = 0; j < 8; j++)
 {
 putchar(a[i][j]);
 }
