624329e8fa7e750852c781f9dcad77cf57f7706a
Andres Medina <andresmedinaph@gmail.com>
2021-10-25 12:06:20 -0500

    Adding Error output

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[])
 int product;
 
 if (argc != 3)
-{
-return (1);
-}
+	printf("Error\n");
+	return (1);
+
 product = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", product);
 return (0);
