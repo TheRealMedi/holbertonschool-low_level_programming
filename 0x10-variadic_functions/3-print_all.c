db1eafd7e263d20126e6fdee092947006ebf34a5
Andres Medina <andresmedinaph@gmail.com>
2021-11-04 17:07:34 -0500

    Adding 3thir file

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all - prints everything
+ * @format: arguments
+ * Return: void
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *args;
+	char *esp = "";
+	va_list params;
+
+	va_start(params, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", esp, va_arg(params, int));
+				break;
+			case 'i':
+				printf("%s%i", esp, va_arg(params, int));
+				break;
+			case 'f':
+				printf("%s%f", esp, va_arg(params, double));
+				break;
+			case 's':
+				args = va_arg(params, char *);
+				if (args == NULL)
+				{
+					args = "(nill)";
+				}
+				printf("%s%s", esp, args);
+				break;
+			default:
+				i++;
+				continue;
+			}
+		esp = ", ";
+		i++;
+		}
+	}
+	printf("\n");
+	va_end(params);
+}
