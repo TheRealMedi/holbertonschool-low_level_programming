1c4d2c89ed6905a8b98f228a62a18d99eb9c802f
Andrés Medina <andresmedinaph@gmail.com>
2022-01-06 12:21:04 -0500

    Addin' dlistint len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * dlistint_len - print acount of nodes in of doubly linked list.
+ * @h: const with header of doubly linked list.
+ * Return: The acount elements.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+    const dlistint_t *backwardHead = h;
+	size_t count = 0;
+
+	while (backwardHead)
+	{
+		backwardHead = backwardHead->next;
+		count++;
+	}
+	return (count);
+}
+
+/*ALTERNATIVE CODE*/
+/*
+ * size_t print_dlistint(const dlistint_t *h)
+ * {
+ *	size_t count;
+ *	for (count = 0; h != NULL; count++)
+ *	{
+ *
+ *		h = h->next;
+ *	}
+ *	return(count);
+ * }
+ */
