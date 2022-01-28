d9557c2c2f7ce9147b408c9b85d2e5ded9377d66
Andres Medina <medios@Andress-MacBook-Pro.local>
2022-01-28 10:47:45 -0500

    Adding hash creation & Library

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table.
+ * @size: Array size.
+ *
+ * Return: If error NULL.
+ *         Otherwise: a pointer to the new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *hash = NULL;
+
+    hash = malloc(sizeof(size));
+    if (!hash)
+        return (NULL);
+    
+    hash->size = size;
+    hash->array = calloc(size, sizeof(hash_node_t *));
+
+    if(!hash->array)
+    {
+        free(hash);
+        return (NULL);
+    }
+
+    return (hash);
+}
